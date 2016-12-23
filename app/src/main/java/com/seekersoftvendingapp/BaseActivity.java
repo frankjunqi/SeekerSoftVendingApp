a9c0b55974ab3d283ed6919e40851f70047262e0
孔建华08490 <kjh08490@ly.com>
2016-12-23 11:54:33 +0800

    fix bug

--- app/src/main/java/com/seekersoftvendingapp/BaseActivity.java
+++ app/src/main/java/com/seekersoftvendingapp/BaseActivity.java
@@ -2,9 +2,11 @@ package com.seekersoftvendingapp;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 /**
@@ -17,6 +19,8 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//竖屏
     }
 
     @Override
